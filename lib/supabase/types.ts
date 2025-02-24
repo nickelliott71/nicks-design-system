96c227a494235614a7cd92c3bb73f3de9d6245f1
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2025-02-24 18:22:13 +0000

    Removed event_type_id

--- lib/supabase/types.ts
+++ lib/supabase/types.ts
@@ -30,7 +30,6 @@ export interface Event {
   release_year: number
   previous_event_id: number | null
   next_event_id: number | null
-  event_type_id?: number
   created_at: string
   publisher?: Publisher
   event_type?: EventType
