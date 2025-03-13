00a7381a85b696600bdfe7f383a53ded59307ceb
nickelliott71 <52502417+nickelliott71@users.noreply.github.com>
2025-03-13 16:27:42 +0000

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,11 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // <-- Add this to disable rule
+    },
+  },
 ];
 
 export default eslintConfig;
